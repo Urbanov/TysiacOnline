db1d9d88d503ec10aca99f6024f2cb001bc9fb40
Urbanov <D.Urbanowicz@stud.elka.pw.edu.pl>
2017-04-26 10:47:50 +0200

    Little changes

--- server/TysiacServer/network/manager.cpp
+++ server/TysiacServer/network/manager.cpp
@@ -12,18 +12,9 @@ void SessionManager::unregisterSession(std::size_t id)
 
 void SessionManager::interpret(const std::string& message)
 {
-	/*// at the moment server broadcasts received message to all connected clients
-	// just for presentation purposes
-	for(auto session : sessions_) {
-		session.second.lock()->write(message);
-	}*/
-	//std::cout << message;
 	auto return_data = game_.doWork(message);
-	//std::cout << "size: " << return_data.size() << std::endl;
 	for (auto& msg : return_data) {
-		//std::cout << msg.first;
 		for(auto id : msg.second) {
-			//std::cout << id << ": " << msg.first << std::endl;
 			sessions_[id].lock()->write(msg.first);
 		}
 	}
